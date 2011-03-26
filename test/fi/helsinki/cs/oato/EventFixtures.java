2f4438dd5938ec7857cbc3926c80f5318728b8c0
Mikko Nylén <mikko.nylen@gmail.com>
2011-03-26 12:54:07 +0200

    Add more fixtures to EventFixtures

--- test/fi/helsinki/cs/oato/EventFixtures.java
+++ test/fi/helsinki/cs/oato/EventFixtures.java
@@ -1,9 +1,13 @@
 package fi.helsinki.cs.oato;
 
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class EventFixtures {
-
+    public static final long ONE_HOUR  = 3600000;
+    public static final long TEN_HOURS = ONE_HOUR * 10;
+    public static final long ONE_DAY   = ONE_HOUR * 24;
+    
     public static Event createDentistAppointment() {
         return new Event(
                 new GregorianCalendar(2011, 3, 25, 16, 0, 0).getTime(),
@@ -20,4 +24,19 @@ public class EventFixtures {
                 "Exactum, CK112");
     }
 
+    public static Event createUpcomingEvent(long millisInFuture) {
+        return new Event(
+                new Date(System.currentTimeMillis()+millisInFuture),
+                new Date(System.currentTimeMillis()+millisInFuture),
+                "Event",
+                "Everywhere");
+    }
+
+    public static Event createPastEvent(long millisInPast) {
+        return new Event(
+                new Date(System.currentTimeMillis()-millisInPast),
+                new Date(System.currentTimeMillis()-millisInPast),
+                "Event",
+                "Everywhere");
+    }
 }

bb0907c6859a7972e69116af44ce59fc5df73713
Mikko Nylén <mikko.nylen@gmail.com>
2011-03-26 13:07:28 +0200

    Wrote javadoc for EventIterator

--- src/fi/helsinki/cs/oato/EventIterator.java
+++ src/fi/helsinki/cs/oato/EventIterator.java
@@ -2,12 +2,22 @@ package fi.helsinki.cs.oato;
 
 import java.util.*;
 
+/**
+ * Iterator over a collection of events. Returns events in ascending order by
+ * the starting date.
+ */
 public class EventIterator implements Iterator<Event> {
     private Event firstEvent;
     private Iterator<Event> innerIterator;
-    
     private boolean discardPast;
 
+    /**
+     * Constructs a new <code>EventIterator</code> for the specified collection.
+     *
+     * @param events the event collection
+     * @param discardPast if <code>true</code>, does not return past events;
+     *                    if <code>false</code>, returns past events
+     */
     public EventIterator(Collection<Event> events, boolean discardPast) {
         List<Event> sortedEvents = new ArrayList<Event>(events);
         Collections.sort(sortedEvents);
@@ -41,11 +51,18 @@ public class EventIterator implements Iterator<Event> {
         return nextEvent;
     }
 
+    /**
+     * @throws UnsupportedOperationException always
+     */
     public void remove() {
         throw new UnsupportedOperationException(
                 "remove() operation is not supported by EventIterator");
     }
 
+    /**
+     * Rewinds the inner iterator to first upcoming event and stores the
+     * event to <code>firstEvent</code> instance variable.
+     */
     private void rewindToFirstUpcomingEvent() {
         long timeMillis = System.currentTimeMillis();
         Event event = null;

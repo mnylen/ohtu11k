019bd8c17ecf7170705a95478960acccde5525a5
Mikko Nylén <mikko.nylen@gmail.com>
2011-03-26 11:55:46 +0200

    Beautify imports section for Schedule -- import java.util.*

--- src/fi/helsinki/cs/oato/Schedule.java
+++ src/fi/helsinki/cs/oato/Schedule.java
@@ -1,9 +1,6 @@
 package fi.helsinki.cs.oato;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Schedule.

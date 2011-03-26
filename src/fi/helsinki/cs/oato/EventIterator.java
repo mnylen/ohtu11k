1bc073d75c377176794c4c6f2c060179aa60ee26
Mikko Nylén <mikko.nylen@gmail.com>
2011-03-26 12:55:27 +0200

    Add EventIterator for iterating over a collection of events in ascending order by date

--- src/fi/helsinki/cs/oato/EventIterator.java
+++ src/fi/helsinki/cs/oato/EventIterator.java
@@ -1,14 +1,31 @@
 package fi.helsinki.cs.oato;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class EventIterator implements Iterator<Event> {
-    private Collection<Event> events;
+    private List<Event> events;
+    private Iterator<Event> innerIterator;
+    
     private boolean discardPast;
 
     public EventIterator(Collection<Event> events, boolean discardPast) {
-        this.events = events;
-        this.discardPast = discardPast;
+        this.events = new ArrayList<Event>(events);
+        Collections.sort(this.events);
+
+        this.discardPast   = discardPast;
+        this.innerIterator = this.events.iterator();
+    }
+
+    public boolean hasNext() {
+        return innerIterator.hasNext();
+    }
+
+    public Event next() {
+        return innerIterator.next();
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException(
+                "remove() operation is not supported by EventIterator");
     }
 }
--- test/fi/helsinki/cs/oato/EventIteratorTest.java
+++ test/fi/helsinki/cs/oato/EventIteratorTest.java
@@ -1,11 +1,41 @@
 package fi.helsinki.cs.oato;
 
-/**
- * Created by IntelliJ IDEA.
- * User: mnylen
- * Date: 3/26/11
- * Time: 12:23
- * To change this template use File | Settings | File Templates.
- */
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class EventIteratorTest {
+    private Event pastEvent;
+    private Event firstUpcomingEvent;
+    private Event secondUpcomingEvent;
+    private Collection<Event> allEvents;
+
+    @Before
+    public void setUp() {
+        this.pastEvent           = EventFixtures.createPastEvent(EventFixtures.ONE_DAY);
+        this.firstUpcomingEvent  = EventFixtures.createUpcomingEvent(EventFixtures.ONE_HOUR);
+        this.secondUpcomingEvent = EventFixtures.createUpcomingEvent(EventFixtures.ONE_DAY);
+
+        this.allEvents = new ArrayList<Event>();
+        this.allEvents.add(this.firstUpcomingEvent);
+        this.allEvents.add(this.pastEvent);
+        this.allEvents.add(this.secondUpcomingEvent);
+    }
+
+    @Test
+    public void testReturnsEventsInOrder() {
+        EventIterator iterator = new EventIterator(this.allEvents, false);
+
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(sameInstance(this.pastEvent)));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(sameInstance(this.firstUpcomingEvent)));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(sameInstance(this.secondUpcomingEvent)));
+        assertThat(iterator.hasNext(), is(false));
+    }
 }

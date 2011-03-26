20d26be665317dbffc389061f8032a6b6aee461e
Mikko Nylén <mikko.nylen@gmail.com>
2011-03-26 11:56:56 +0200

    Add javadoc for Schedule constructors

--- src/fi/helsinki/cs/oato/Schedule.java
+++ src/fi/helsinki/cs/oato/Schedule.java
@@ -8,10 +8,17 @@ import java.util.*;
 public class Schedule {
     private Collection<Event> events;
 
+    /**
+     * Constructs an empty schedule.
+     */
     public Schedule() {
         this.events = new ArrayList<Event>();
     }
 
+    /**
+     * Constructs a schedule with specified events.
+     * @param events the events
+     */
     public Schedule(Collection<Event> events) {
         this.events = new ArrayList<Event>(events);
     }
